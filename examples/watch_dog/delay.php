5cd82881e9490d8441896af0255698011fd6c13f
twosee <twose@qq.com>
2020-09-20 22:53:57 +0800

    Add delay example

---
+++ examples/watch_dog/delay.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of Swow
+ *
+ * @link     https://github.com/swow/swow
+ * @contact  twosee <twosee@php.net>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+use Swow\Coroutine;
+use Swow\Sync\WaitReference;
+use Swow\WatchDog;
+
+/* if CPU starvation occurred, kill the coroutine */
+WatchDog::run(0, 0, function () { exit; });
+
+$wf = new WaitReference();
+Coroutine::run(function () use ($wf) {
+    $count = 0;
+    while (true) {
+        $count++;
+    }
+});
+WaitReference::wait($wf);
+echo "Exited\n";
