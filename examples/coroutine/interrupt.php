3a7422de6402c700da3f6b271f455fd87d3dfbcb
twosee <twose@qq.com>
2020-10-11 17:17:22 +0800

    Add examples: coroutine_interrupt, buffer_vs_php_stream

---
+++ examples/coroutine/interrupt.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Swow
+ *
+ * @link     https://github.com/swow/swow
+ * @contact  twosee <twosee@php.net>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+use Swow\Coroutine;
+
+$coroutine = Coroutine::run(function() {
+    while (true) {
+        try {
+            var_dump(sleep(999));
+        } catch (Exception $e) {
+            // 忽略
+        }
+    }
+});
+$coroutine->resume(); // 强制唤醒中断睡眠
+$coroutine->throw(new Exception); // 向协程抛出异常
+$coroutine->kill(); // 杀死协程
