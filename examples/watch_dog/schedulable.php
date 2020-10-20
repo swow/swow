8cc3cd28c2a9a8c783816896d5035962a7fce04c
twosee <twose@qq.com>
2020-10-20 19:31:16 +0800

    Add a example for watch dog

---
+++ examples/watch_dog/schedulable.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of Swow
+ *
+ * @link     https://github.com/swow/swow
+ * @contact  twosee <twosee@php.net>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+/* @noinspection PhpUnreachableStatementInspection */
+
+declare(strict_types=1);
+
+use Swow\Coroutine;
+use Swow\WatchDog;
+
+// 0.1s/t
+WatchDog::run(100 * 1000 * 1000, 0, function () {
+    $coroutine = Coroutine::getCurrent();
+    $coroutine->__alert_count = ($coroutine->__alert_count ?? 0) + 1;
+    echo 'CPU starvation occurred, suspend this coroutine...' . PHP_EOL;
+    sleep(0);
+    if ($coroutine->__alert_count > 5) {
+        $coroutine->kill();
+    }
+});
+
+Coroutine::run(function () {
+    $s = microtime(true);
+    for ($n = 5; $n--;) {
+        echo 'Do something...' . PHP_EOL;
+        usleep(1000);
+    }
+    $s = microtime(true) - $s;
+    echo "Use {$s} ms" . PHP_EOL;
+});
+
+$count = 0;
+try {
+    while (true) {
+        $count++;
+    }
+} finally {
+    echo "Count is {$count}" . PHP_EOL;
+}
