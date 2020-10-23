e8d19731de85b56dea6f2a31b798b02122c7553d
李铭昕 <715557344@qq.com>
2020-10-23 09:14:16 +0800

    Added HTTP Client

---
+++ lib/Swow/Http/Client/ClientException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of Swow
+ *
+ * @link     https://github.com/swow/swow
+ * @contact  twosee <twosee@php.net>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+namespace Swow\Http\Client;
+
+use Psr\Http\Client\ClientExceptionInterface;
+use Swow\Socket\Exception;
+
+class ClientException extends Exception implements ClientExceptionInterface
+{
+}
