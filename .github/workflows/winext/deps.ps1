59117581100470071162968c3d52643ef4a97043
dixyes <dixyes@gmail.com>
2021-01-31 21:29:34 +0800

    Initial implement for automatic dlls build

--- .github/workflows/winext/deps.ps1
+++ .github/workflows/winext/deps.ps1
@@ -4,28 +4,15 @@ param (
     [Object[]]$DllDeps,
     [int]$MaxTry = 3,
     [string]$ToolsPath = "C:\tools\phpdev",
-    [string]$PhpBin = "php"
+    [string]$PhpBin = "php",
+    [string]$PhpVer = "",
+    [string]$PhpVCVer = "",
+    [string]$PhpArch = "x64",
+    [bool]$PhpTs = $false
 )
 
 $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
-. "$scriptPath\logger.ps1" -ToolName "deps"
-
-function dl{
-    param (
-        $Uri, $Dest
-    )
-
-    for ($i=0; $i -lt $MaxTry; $i++){
-        try{
-            Invoke-WebRequest -Uri $Uri -OutFile $Dest
-            return 'ok'
-        }catch [System.Net.WebException],[System.IO.IOException]{
-            warn "Failed download ${Uri}, try again."
-            continue
-        }
-    }
-    return 'failed'
-}
+. "$scriptPath\utils.ps1" -ToolName "deps" -MaxTry $MaxTry
 
 if($DllDeps.Length -Lt 1){
     warn "No deps specified, just skipped."
@@ -33,82 +20,75 @@ if($DllDeps.Length -Lt 1){
 }
 
 # things we used later
-try{
-    $phpver = & $PhpBin -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . PHP_EOL;"
-    $phpinfo = & $PhpBin -i
-    $phpvcver = ($phpinfo | Select-String -Pattern 'PHP Extension Build => .+,(.+)' -CaseSensitive -List).Matches.Groups[1]
-    $phparch = ($phpinfo | Select-String -Pattern 'Architecture => (.+)' -CaseSensitive -List).Matches.Groups[1]
-}finally{
-    if(
-        !$phpver -Or
-        !$phpvcver -Or
-        !$phparch
-    ){
-        err "Cannot determine php attributes, do you have php in PATH?"
-        warn "phpver: $phpver"
-        warn "phpvcver: $phpvcver"
-        warn "phparchver: $phparchver"
+if ("".Equals($PhpVer)){
+    try{
+        $PhpVer = & $PhpBin -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . PHP_EOL;"
+        $phpinfo = & $PhpBin -i
+        $PhpVCVer = ($phpinfo | Select-String -Pattern 'PHP Extension Build => .+,(.+)' -CaseSensitive -List).Matches.Groups[1]
+        $PhpArch = ($phpinfo | Select-String -Pattern 'Architecture => (.+)' -CaseSensitive -List).Matches.Groups[1]
+    }finally{
+        if(
+            !$PhpVer -Or
+            !$PhpVCVer -Or
+            !$PhpArch
+        ){
+            err "Cannot determine php attributes, do you have php in PATH?"
+            warn "phpver: $PhpVer"
+            warn "phpvcver: $PhpVCVer"
+            warn "phparch: $PhpArch"
+            exit 1
+        }
+    }
+}
+
+info "Try to fetch deps series list from windows.php.net"
+$series = (fetchinfo "https://windows.php.net/downloads/php-sdk/deps/series/packages-$PhpVer-$PhpVCVer-$PhpArch-staging.txt").Content
+if(!$series){
+    warn "Cannot get series information from windows.php.net, try file list instead"
+    $filelist = (fetchinfo ("https://windows.php.net/downloads/php-sdk/deps/" + $PhpVCVer.ToLower() + "/$PhpArch/")).Content
+    if(!$filelist){
+        err "Neither series file nor file list can be got, aborting"
         exit 1
     }
 }
 
-for ($i=0; $i -lt $MaxTry; $i++){
-    try{
-        info "Try to fetch deps list from windows.php.net"
-        $filelist = (Invoke-WebRequest `
-            -Uri "https://windows.php.net/downloads/php-sdk/deps/series/packages-$phpver-$phpvcver-$phparch-staging.txt").Content
-    }catch [System.Net.WebException],[System.IO.IOException]{
-        warn "Failed to fetch deps list from windows.php.net"
-        Write-Host $_
-        continue
-    }
-    if($filelist){
-        break
-    }
-}
-if(!$filelist){
-    err "Cannot get deps information from windows.php.net"
-    exit 1
-}
-
 foreach ($depname in $DllDeps) {
     $depfile = $null
-    foreach ($filename in $filelist.Split()) {
-        if($filename.StartsWith($depname)){
-            info "Found file $filename for dep $depname"
-            $depfile = $filename
-            break
+    if($series){
+        foreach ($filename in $series.Split()) {
+            if($filename.StartsWith($depname)){
+                info "Found file $filename for dep $depname"
+                $depfile = $filename
+                break
+            }
         }
+    }else{
+        $depfile = searchfile $filelist -Pattern ("$depname-:-" + $PhpVCVer.ToLower() + "-$PhpArch.zip")
     }
+    
     if(!$depfile){
         err "Cannot find dep file for $depname"
         exit 1
     }
     info "Downloading $filename from windows.php.net"
-    if (-Not (Test-Path -Path $ToolsPath -PathType Container)){
-        info "Creating dir" $ToolsPath
-        New-Item -Path $ToolsPath -ItemType Container | Out-Null
-    }
-    if (-Not (Test-Path -Path $ToolsPath\deps -PathType Container)){
-        info "Creating dir $ToolsPath\deps"
-        New-Item -Path $ToolsPath\deps -ItemType Container | Out-Null
-    }
+    provedir "$ToolsPath"
+    provedir "$ToolsPath\deps"
     $dest = "$ToolsPath\deps\$depfile"
     if(Test-Path $dest -PathType Leaf){
         warn "$depfile is already provided, instant extract it."
     }else {
-        $uri = "https://windows.php.net/downloads/php-sdk/deps/$phpvcver/$phparch/$depfile"
-        $ret = dl -Uri $uri -Dest $dest
-        if (-Not "ok".Equals($ret)){
+        $uri = "https://windows.php.net/downloads/php-sdk/deps/$PhpVCVer/$PhpArch/$depfile"
+        $ret = dlwithhash -Uri $uri -Dest $dest
+        if (!$ret){
             err "Failed download $uri."
             exit 1
         }
     }
     info "Unzipping $dest to $ToolsPath\deps\"
+    
     Expand-Archive $dest -Destination "$ToolsPath\deps\" -Force
-    info "Install dlls and pdbs into $phpexedir"
-    Copy-Item -Path "$ToolsPath\deps\bin\*.dll" -Destination $phpexedir
-    Copy-Item -Path "$ToolsPath\deps\bin\*.pdb" -Destination $phpexedir
 }
 
 info "Done."
+
+exit 0
--- .github/workflows/winext/devpack.ps1
+++ .github/workflows/winext/devpack.ps1
@@ -3,74 +3,58 @@
 param (
     [int]$MaxTry = 3,
     [string]$ToolsPath = "C:\tools\phpdev",
-    [string]$PhpBin = "php"
+    [string]$PhpBin = "php",
+    [string]$PhpVer = "",
+    [string]$PhpVCVer = "",
+    [string]$PhpArch = "x64",
+    [bool]$PhpTs = $false
 )
 
 $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
-. "$scriptPath\logger.ps1" -ToolName "devpack"
+. "$scriptPath\utils.ps1" -ToolName "devpack" -MaxTry $MaxTry
 
-# things we used later
-try{
-    $phpver = & $PhpBin -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . PHP_EOL;"
-    $phpinfo = & $PhpBin -i
-    $phpvcver = ($phpinfo | Select-String -Pattern 'PHP Extension Build => .+,(.+)' -CaseSensitive -List).Matches.Groups[1]
-    $phparch = ($phpinfo | Select-String -Pattern 'Architecture => (.+)' -CaseSensitive -List).Matches.Groups[1]
-    $phpvar = & $PhpBin -r "echo (PHP_ZTS?:'n') . 'ts-$phpvcver-$phparch' . PHP_EOL;"
-}finally{
-    if(
-        !$phpver -Or
-        !$phpvcver -Or
-        !$phparch -Or
-        !$phpvar
-    ){
-        err "Cannot determine php attributes, do you have php in PATH?"
-        warn "phpver: $phpver"
-        warn "phpvcver: $phpvcver"
-        warn "phparchver: $phparchver"
-        warn "phpvar: $phpvar"
-        exit 1
+if ("".Equals($PhpVer)){
+    try{
+        $PhpVer = & $PhpBin -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION . PHP_EOL;"
+        $phpinfo = & $PhpBin -i
+        $PhpVCVer = ($phpinfo | Select-String -Pattern 'PHP Extension Build => .+,(.+)' -CaseSensitive -List).Matches.Groups[1]
+        $PhpArch = ($phpinfo | Select-String -Pattern 'Architecture => (.+)' -CaseSensitive -List).Matches.Groups[1]
+        $phpvar = & $PhpBin -r "echo (PHP_ZTS?:'n') . 'ts-$PhpVCVer-$PhpArch' . PHP_EOL;"
+        $dashnts = & $PhpBin -r "echo (PHP_ZTS?'':'-nts') . PHP_EOL;"
+        $underscorets = & $PhpBin -r "echo (PHP_ZTS?'_TS':'') . PHP_EOL;"
+    }finally{
+    }
+}else{
+    if($PhpTs){
+        $phpvar = "ts-$PhpVCVer-$PhpArch"
+        $dashnts = ""
+        $underscorets = "_TS"
+    }else{
+        $phpvar = "nts-$PhpVCVer-$PhpArch"
+        $dashnts = "-nts"
+        $underscorets = ""
     }
 }
-
-function provedir {
-    if (-Not (Test-Path -Path $ToolsPath -PathType Container)){
-        info "Creating dir" $ToolsPath
-        New-Item -Path $ToolsPath -ItemType Container | Out-Null
-    }
+if(
+    !$PhpVer -Or
+    !$PhpVCVer -Or
+    !$PhpArch -Or
+    !$phpvar
+){
+    err "Cannot determine php attributes, do you have php in PATH?"
+    err "You can also specify vals via arguments"
+    warn "phpver: $PhpVer"
+    warn "phpvcver: $PhpVCVer"
+    warn "phparch: $PhpArch"
+    warn "phpvar: $phpvar"
+    exit 1
 }
 
-function dlwithhash{
-    param (
-        $Uri, $Dest, $Hash, $Hashmethod
-    )
 
-    for ($i=0; $i -lt $MaxTry; $i++){
-        try{
-            info "Try to download ${uri}"
-            Invoke-WebRequest -Uri $Uri -OutFile $Dest | Out-Null
-        }catch [System.Net.WebException],[System.IO.IOException]{
-            warn "Failed download ${uri}."
-            Write-Host $_
-            continue
-        }
-        
-        if($Hashmethod -And -Not $Hash -Eq (Get-FileHash $Dest -Algorithm $Hashmethod).Hash ){
-            warn "Bad checksum, remove file $Dest."
-            Remove-Item $Dest | Out-Null
-            continue
-        }
-        break
-    }
-    if ($hashmethod -And -Not $hash -Eq (Get-FileHash $dest -Algorithm $Hashmethod).Hash ){
-        warn "Cannot download ${uri}: bad checksum."
-        return "failed"
-    }
-    return "ok"
-}
 function fetchdevpack(){
-    if ($info.$phpver.$phpvar) {
+    if ($info.$PhpVer.$phpvar) {
         info "Found target version on releases, try using latest devpack."
-        $latest = $info.$phpver.$phpvar."devel_pack"
+        $latest = $info.$PhpVer.$phpvar."devel_pack"
         if($latest.sha1){
             $hash = $latest.sha1
             $hashmethod = "SHA1"
@@ -89,41 +73,47 @@ function fetchdevpack(){
             }
         }
         
-        provedir
+        provedir $ToolsPath
         $ret = dlwithhash `
             -Uri ("https://windows.php.net/downloads/releases/" + ($latest.path)) `
             -Dest $dest `
             -Hash $hash `
             -Hashmethod $hashmethod
-        if ("ok".Equals($ret)){
+        if ($ret){
             return $dest
         }
     } else {
-        info "Target version is not active release or failed to download releases info, try fetch instantly."
-        $fn = & $PhpBin -r "echo 'php-devel-pack-' . PHP_VERSION . '-' . (PHP_ZTS?'nts-':'') . 'Win32-$phpvcver-$phparch.zip' . PHP_EOL;"
-        provedir
+        info "Target version is not active release or failed to download releases info, try search in file list."
+        try{
+            $page = fetchinfo "https://windows.php.net/downloads/releases/archives/"
+            $groups = ($page | Select-String `
+                -List `
+                -AllMatches `
+                -Pattern ('<A HREF="[^"]+?">(?:php-devel-pack-' + $PhpVer + '.(?:\d+)' + $dashnts +'-Win32-' + $PhpVCVer + '-' + $PhpArch + '.zip)</A>')).Matches.Groups
+            $used = 0
+            foreach ($item in $groups) {
+                $minor = ($item | Select-String -Pattern ($PhpVer + '.(\d+)')).Matches.Groups[1].ToString() -As "int"
+                if ($minor -Gt $used){
+                    $used = $minor
+                }
+            }
+            $fn = 'php-devel-pack-' + $PhpVer + '.' + $used + $dashnts +'-Win32-' + $PhpVCVer + '-' + $PhpArch + '.zip'
+        }catch [System.Net.WebException],[System.IO.IOException]{
+            warn "Cannot fetch archives list, use oldest instead"
+            Write-Host $_
+            $fn = 'php-devel-pack-' + $PhpVer + '.0' + $dashnts +'-Win32-' + $PhpVCVer + '-' + $PhpArch + '.zip'
+        }
+        #Write-Host $fn
+        provedir $ToolsPath
         $ret = dlwithhash -Uri "https://windows.php.net/downloads/releases/archives/$fn" -Dest "$ToolsPath\$fn"
-        if ("ok".Equals($ret)){
+        if ($ret){
             return "$ToolsPath\$fn"
         }
     }
 }
 
-info "Finding devpack for PHP $phpver $phpvar"
-
-for ($i=0; $i -lt $MaxTry; $i++){
-    try{
-        $info = Invoke-WebRequest -Uri "https://windows.php.net/downloads/releases/releases.json" | ConvertFrom-Json
-    }catch [System.Net.WebException],[System.IO.IOException]{
-        warn "Failed to fetch php releases info from windows.php.net, try again."
-        Write-Host $_
-        continue
-    }
-    #$info = Invoke-WebRequest -Uri "https://on" | ConvertFrom-Json
-    if($info){
-        break
-    }
-}
+info "Finding devpack for PHP $PhpVer $phpvar"
+$info = fetchinfo -Uri "https://windows.php.net/downloads/releases/releases.json" | ConvertFrom-Json
 if(!$info){
     warn "Cannot fetch php releases info from windows.php.net."
 }
@@ -150,13 +140,14 @@ info "Done unzipping devpack, generate env.bat."
 
 # Since setup-php only provides Release version PHP, yet we only support Release
 # Maybe sometimes we can build PHP by ourself?
-$phpts = & $PhpBin -r "echo PHP_ZTS?'_TS':'';"
 $content="
 @ECHO OFF
-SET BUILD_DIR=$phparch\Release$phpts
+SET BUILD_DIR=$PhpArch\Release$underscorets
 SET PATH=$ToolsPath\$dirname;%PATH%
-$ToolsPath\php-sdk-binary-tools\phpsdk-starter.bat -c $phpvcver -a $phparch -t %*
+$ToolsPath\php-sdk-binary-tools\phpsdk-starter.bat -c $PhpVCVer -a $PhpArch -t %*
 "
-$content | Out-File -Encoding UTF8 -FilePath $ToolsPath\env.bat
+[IO.File]::WriteAllLines("$ToolsPath\env.bat", $content)
 
 info "Done preparing dev-pack"
+
+exit 0
