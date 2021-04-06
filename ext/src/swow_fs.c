7fa26c8fae9544b9429e1e63f36e18b0729fc7e7
dixyes <dixyes@gmail.com>
2021-04-06 17:28:47 +0800

    Fix mem leak when some fs op failed

--- ext/src/swow_fs.c
+++ ext/src/swow_fs.c
@@ -329,6 +329,7 @@ struct utimbuf {
     new_state.cwd = virtual_getcwd_ex(&new_state.cwd_length); \
     /* virtual_file_ex returns non-zero(1 or -1) for error */ \
     if (NULL == new_state.cwd || 0 != virtual_file_ex(&new_state, path, NULL, flags)) {\
+        if(new_state.cwd) efree(new_state.cwd);\
         {failed}\
         break;\
     }\
