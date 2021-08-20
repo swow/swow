5c1c264a8a9111cef3ad95395e09451d471e6198
twosee <twose@qq.com>
2021-08-20 09:44:32 +0800

    Fix missing Http\Server\Connection and add a test for mixed HTTP server

---
+++ lib/tests/Http/ServerTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of Swow
+ *
+ * @link     https://github.com/swow/swow
+ * @contact  twosee <twosee@php.net>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+namespace SwowTest\Http;
+
+use PHPUnit\Framework\TestCase;
+use Swow\Coroutine;
+use Swow\Http\Client as HttpClient;
+use Swow\Http\Request as HttpRequest;
+use Swow\Http\WebSocketFrame;
+use function Swow\defer;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ServerTest extends TestCase
+{
+    public function testMixedServer()
+    {
+        $mixedServerFile = __DIR__ . '/../../../examples/http_server/mixed.php';
+        if (!file_exists($mixedServerFile)) {
+            $this->markTestSkipped('aaa');
+        }
+        $serverCoroutine = Coroutine::run(function () use ($mixedServerFile) {
+            require $mixedServerFile;
+        });
+        // so hacky ^^
+        /* @var Server $server */
+        $serverCoroutine->eval('$GLOBALS[\'server\'] = $this;');
+        $server = $GLOBALS['server'];
+        defer(function () use ($server) {
+            $server->close();
+        });
+
+        /* HTTP */
+        $client = new HttpClient();
+        $request = new HttpRequest();
+        $request
+            ->setUriString('/echo')
+            ->getBody()
+            ->write('Hello Swow');
+        $response = $client
+            ->connect($server->getSockAddress(), $server->getSockPort())
+            ->sendRequest($request);
+        $this->assertSame('Hello Swow', $response->getBodyAsString());
+
+        /* WebSocket */
+        $request = new HttpRequest('GET', '/chat');
+        $client->upgradeToWebSocket($request);
+        /* chat */
+        for ($n = 1; $n <= 3; $n++) {
+            $message = new WebSocketFrame();
+            $message->getPayloadData()->write("Hello Swow {$n}");
+            $reply = $client
+                ->sendWebSocketFrame($message)
+                ->recvWebSocketFrame();
+            $this->assertStringContainsString("Hello Swow {$n}", $reply->getPayloadDataAsString());
+        }
+    }
+}
