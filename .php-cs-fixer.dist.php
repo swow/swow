f183d99f5aaf3561f9f5e25ca83438fa2a442ed4
twosee <twose@qq.com>
2021-12-17 19:25:12 +0800

    Remove extra space in header comment

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,8 +2,8 @@
 /**
  * This file is part of Swow
  *
- * @link     https://github.com/swow/swow
- * @contact  twosee <twosee@php.net>
+ * @link    https://github.com/swow/swow
+ * @contact twosee <twosee@php.net>
  *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code
@@ -14,8 +14,8 @@ declare(strict_types=1);
 $header = <<<'TEXT'
 This file is part of Swow
 
-@link     https://github.com/swow/swow
-@contact  twosee <twosee@php.net>
+@link    https://github.com/swow/swow
+@contact twosee <twosee@php.net>
 
 For the full copyright and license information,
 please view the LICENSE file that was distributed with this source code
