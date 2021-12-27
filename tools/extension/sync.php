c2d32b2d23fa389927022510bbaabc4994f34731
twosee <twose@qq.com>
2021-12-27 11:27:31 +0800

    Simplify sync script

--- tools/extension/sync.php
+++ tools/extension/sync.php
@@ -9,18 +9,104 @@
  * please view the LICENSE file that was distributed with this source code
  */
 
-require __DIR__ . '/../tools.php';
-require __DIR__ . '/DependencyManager.php';
-
-use Swow\Tools\DependencyManager;
+use function Swow\Tools\defer;
 use function Swow\Tools\error;
 use function Swow\Tools\log;
 use function Swow\Tools\notice;
 use function Swow\Tools\ok;
+use Swow\Util\FileSystem;
+
+require __DIR__ . '/../tools.php';
 
 $workSpace = __DIR__ . '/../../ext/deps';
 
-$syncFromGit = function (string $orgName, string $repoName, string $sourceDir, string $targetDir, array $requires = []): array {
+$sysTempDir = (function (): string {
+    if (is_writable('/tmp')) {
+        return '/tmp';
+    } else {
+        $tempDir = sys_get_temp_dir();
+        notice('Sys temp dir is redirected to ' . $tempDir);
+        return $tempDir;
+    }
+})();
+
+$sync = function (string $name, string $url, string $sourceDir, string $targetDir, array $requires = []) use ($sysTempDir): string
+{
+    if (PHP_OS_FAMILY === 'Windows') {
+        throw new RuntimeException('Linux only');
+    }
+    $targetWorkspace = dirname($targetDir);
+    if (file_exists($targetDir) && !`cd {$targetWorkspace} && git rev-parse HEAD`) {
+        throw new RuntimeException("Unable to find git repo in {$targetDir}");
+    }
+    $tmpBackupDir = $sysTempDir . '/swow_deps_backup_' . date('YmdHis');
+    if (!is_dir($tmpBackupDir) && !mkdir($tmpBackupDir, 0755)) {
+        throw new RuntimeException("Unable to create tmp backup dir '{$tmpBackupDir}'");
+    }
+    if (str_ends_with($url, '.git') || (parse_url($url)['scheme'] ?? '') === 'file') {
+        $tmpSourceDir = $sysTempDir . "/swow_deps/{$name}";
+        if (is_dir($tmpSourceDir)) {
+            FileSystem::remove($tmpSourceDir);
+        }
+        if (!mkdir($tmpSourceDir, 0755, true)) {
+            throw new RuntimeException("Unable to create tmp source dir '{$tmpSourceDir}'");
+        }
+        defer($defer, function () use ($tmpSourceDir) {
+            if (is_dir($tmpSourceDir)) {
+                FileSystem::remove($tmpSourceDir);
+            }
+        });
+        `git clone --depth=1 {$url} {$tmpSourceDir}`;
+        if (!is_dir("{$tmpSourceDir}/.git")) {
+            throw new RuntimeException("Clone source files of {$name} failed");
+        }
+        $version = trim(`cd {$tmpSourceDir} && git rev-parse HEAD`);
+        if (strlen($version) !== 40) {
+            throw new RuntimeException("Invalid git version {$version}");
+        }
+        FileSystem::remove("{$tmpSourceDir}/.git");
+    } else {
+        throw new RuntimeException('Unsupported now');
+    }
+    if (file_exists($targetDir)) {
+        try {
+            FileSystem::move($targetDir, $tmpBackupDir);
+        } catch (FileSystem\IOException $exception) {
+            throw new RuntimeException("Backup from '{$targetDir}' to '{$tmpBackupDir}' failed: {$exception->getMessage()}");
+        }
+    }
+    if (count($requires) > 0) {
+        if (!mkdir($targetDir, 0755, true)) {
+            throw new RuntimeException("Unable to create target source dir '{$targetDir}'");
+        }
+        foreach ($requires as $require) {
+            $tmpSourceFile = "{$tmpSourceDir}/{$require}";
+            $targetFile = "{$targetDir}/{$require}";
+            try {
+                FileSystem::move($tmpSourceFile, $targetFile);
+            } catch (FileSystem\IOException $exception) {
+                throw new RuntimeException("Update dep files from '{$tmpSourceFile}' to {$targetFile} failed: {$exception->getMessage()}");
+            }
+        }
+    } else {
+        $targetParentDir = dirname($targetDir);
+        if (!is_dir($targetParentDir)) {
+            if (!mkdir($targetParentDir, 0755, true)) {
+                throw new RuntimeException("Unable to create target parent dir '{$targetParentDir}'");
+            }
+        }
+        try {
+            FileSystem::move($tmpSourceDir, $targetDir);
+        } catch (FileSystem\IOException $exception) {
+            throw new RuntimeException("Update dep files from '{$tmpSourceDir}' to {$targetDir} failed: {$exception->getMessage()}");
+        }
+    }
+    `cd {$targetDir} && git add --ignore-errors -A`;
+
+    return $version;
+};
+
+$syncFromGit = function (string $orgName, string $repoName, string $sourceDir, string $targetDir, array $requires = []) use ($sync): array {
     $fullName = "{$orgName}/{$repoName}";
     log("Sync {$fullName}...");
     $url = null;
@@ -40,7 +126,7 @@ $syncFromGit = function (string $orgName, string $repoName, string $sourceDir, s
     if ($url === null) {
         $url = "git@github.com:{$fullName}.git";
     }
-    $version = DependencyManager::sync($repoName, $url, $sourceDir, $targetDir, $requires);
+    $version = $sync($repoName, $url, $sourceDir, $targetDir, $requires);
     ok("Sync {$fullName} to {$version}");
     return [$repoName => "* {$fullName}@{$version}\n"];
 };
@@ -48,9 +134,9 @@ $syncFromGit = function (string $orgName, string $repoName, string $sourceDir, s
 try {
     $deps = [];
     $deps += $syncFromGit(
-        'libcat', 'libcat',
-        'libcat', "{$workSpace}/libcat",
-        ['include', 'src', 'deps', 'tools', 'clean.sh', 'LICENSE']
+        orgName: 'libcat', repoName: 'libcat',
+        sourceDir: 'libcat', targetDir: "{$workSpace}/libcat",
+        requires: ['include', 'src', 'deps', 'tools', 'clean.sh', 'LICENSE']
     );
 } catch (Exception $exception) {
     error($exception->getMessage());
