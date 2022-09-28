7e96ca1b092e0eccda595080b43f50432d7b198b
twosee <twose@qq.com>
2022-09-29 01:20:05 +0800

    Add FIXME comment

--- lib/swow-library/src/Psr7/Message/ResponseException.php
+++ lib/swow-library/src/Psr7/Message/ResponseException.php
@@ -16,6 +16,9 @@ namespace Swow\Psr7\Message;
 use Swow\Http\Status;
 use Throwable;
 
+/**
+ * FIXME: this class should be removed
+ */
 class ResponseException extends \Swow\Exception
 {
     public function __construct(int $statusCode, string $reasonPhrase = '', ?Throwable $previous = null)
