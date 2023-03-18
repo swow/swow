e1a0ef8fb3f7a037a082366d7dfd4ff0570b1c63
pandaLIU <563883861@qq.com>
2023-03-18 15:02:01 +0800

    Updated ServerConnection SendHttpFile test case

--- lib/swow-library/tests/Psr7/Server/ServerConnectionTest.php
+++ lib/swow-library/tests/Psr7/Server/ServerConnectionTest.php
@@ -21,8 +21,6 @@ use Swow\Sync\WaitReference;
 
 use const CURLOPT_HEADER;
 use const CURLOPT_RETURNTRANSFER;
-use const CURLOPT_SSL_VERIFYHOST;
-use const CURLOPT_SSL_VERIFYPEER;
 use const CURLOPT_URL;
 
 /**
@@ -31,38 +29,63 @@ use const CURLOPT_URL;
  */
 final class ServerConnectionTest extends TestCase
 {
+    protected string $tempFile = '';
+
+    protected string $tempDir = '';
+
+    protected function setUp(): void
+    {
+        $tempDir = __DIR__ . '/temp';
+
+        if (!is_dir($tempDir)) {
+            mkdir($tempDir);
+        }
+
+        $this->tempDir = $tempDir;
+
+        $fileName = uniqid() . '.txt';
+        $this->tempFile = $tempDir . '/' . $fileName;
+        file_put_contents($this->tempFile, uniqid());
+    }
+
+    protected function tearDown(): void
+    {
+        unlink($this->tempFile);
+        rmdir($this->tempDir);
+        $this->tempFile = '';
+        $this->tempDir = '';
+    }
+
     public function testSendHttpFile(): void
     {
-        $logoPath = __DIR__ . '/swow.svg';
         $server = new Server();
         $server->bind('127.0.0.1')->listen();
 
+        $tempFile = $this->tempFile;
         $wr = new WaitReference();
-        Coroutine::run(static function () use ($server, $logoPath, $wr): void {
+        Coroutine::run(static function () use ($server, $tempFile, $wr): void {
             $connection = $server->acceptConnection();
             $connection->recvHttpRequest();
             $response = Psr7::createResponse();
-            $connection->sendHttpFile($response, $logoPath);
+            $connection->sendHttpFile($response, $tempFile);
         });
 
         $url = sprintf('%s:%s', $server->getSockAddress(), $server->getSockPort());
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, true);
         $response = curl_exec($ch);
         curl_close($ch);
 
-        $logoSize = filesize($logoPath);
+        $logoSize = filesize($this->tempFile);
         [$headers, $body] = explode("\r\n\r\n", $response);
         $expected =
             "HTTP/1.1 200 OK\r\n" .
-            "Content-Type: image/svg+xml\r\n" .
+            "Content-Type: text/plain\r\n" .
             "Content-Length: {$logoSize}";
         $this->assertEquals($expected, $headers);
-        $this->assertEquals(file_get_contents($logoPath), $body);
+        $this->assertEquals(file_get_contents($this->tempFile), $body);
 
         $wr::wait($wr);
         $server->close();
