8dc99a2acb16581e3e4ec833ff86b0973120bb60
twosee <twose@qq.com>
2023-05-30 18:45:46 +0800

    Make sendHttpFile test more robust

--- lib/swow-library/tests/Psr7/Server/ServerConnectionTest.php
+++ lib/swow-library/tests/Psr7/Server/ServerConnectionTest.php
@@ -15,17 +15,23 @@ namespace Swow\Tests\Psr7\Server;
 
 use PHPUnit\Framework\TestCase;
 use Swow\Coroutine;
+use Swow\Http\Status;
 use Swow\Psr7\Psr7;
 use Swow\Psr7\Server\Server;
 use Swow\Sync\WaitReference;
 
+use function array_map;
+use function is_numeric;
+use function Swow\TestUtils\getRandomBytes;
+
 use const CURLOPT_HEADER;
+use const CURLOPT_PROXY;
 use const CURLOPT_RETURNTRANSFER;
 use const CURLOPT_URL;
 
 /**
  * @internal
- * @coversNothing
+ * @covers \Swow\Psr7\Server\ServerConnection::sendHttpFile()
  */
 final class ServerConnectionTest extends TestCase
 {
@@ -45,7 +51,7 @@ final class ServerConnectionTest extends TestCase
 
         $fileName = uniqid() . '.txt';
         $this->tempFile = $tempDir . '/' . $fileName;
-        file_put_contents($this->tempFile, uniqid());
+        file_put_contents($this->tempFile, getRandomBytes(8192));
     }
 
     protected function tearDown(): void
@@ -75,17 +81,30 @@ final class ServerConnectionTest extends TestCase
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_PROXY, false);
         $response = curl_exec($ch);
         curl_close($ch);
 
-        $logoSize = filesize($this->tempFile);
-        [$headers, $body] = explode("\r\n\r\n", $response);
-        $expected =
-            "HTTP/1.1 200 OK\r\n" .
-            "Content-Type: text/plain\r\n" .
-            "Content-Length: {$logoSize}";
-        $this->assertEquals($expected, $headers);
-        $this->assertEquals(file_get_contents($this->tempFile), $body);
+        $fileSize = filesize($this->tempFile);
+        [$headerLines, $body] = explode("\r\n\r\n", $response);
+        // parse headers to array
+        $headerLines = explode("\r\n", $headerLines);
+        $headLine = array_shift($headerLines);
+        $headLine = explode(' ', $headLine, 3);
+        if (!isset($headLine[1]) || !is_numeric($headLine[1])) {
+            $this->fail('Invalid response');
+        }
+        $status = (int) $headLine[1];
+        $headers = [];
+        foreach ($headerLines as $headerLine) {
+            $headerLineParts = array_map('trim', explode(':', $headerLine, 2));
+            $headers[$headerLineParts[0]] = $headerLineParts[1] ?? '';
+        }
+        $this->assertSame(Status::OK, $status);
+
+        $this->assertSame('text/plain', $headers['Content-Type'] ?? '');
+        $this->assertSame($fileSize, (int) ($headers['Content-Length'] ?? ''));
+        $this->assertSame(file_get_contents($this->tempFile), $body);
 
         $wr::wait($wr);
         $server->close();
