5e8fc644ae15e885903846e9479f01fbd1e62f47
twosee <twose@qq.com>
2023-11-12 01:03:21 +0800

    Make php-wrapper smarter
    Php-wrapper will filter those extensions that have been configured.

--- tools/php-wrapper.php
+++ tools/php-wrapper.php
@@ -13,9 +13,20 @@
 declare(strict_types=1);
 
 (static function () use ($argv): void {
-    $args = array_slice($argv, 1);
+    $options = getopt('d:');
+    $requiredExtensions = [];
+    if (isset($options['d'])) {
+        $commandLoadedExtensions = is_array($options['d']) ? $options['d'] : [$options['d']];
+        foreach ($commandLoadedExtensions as $commandLoadedExtension) {
+            if (str_starts_with($commandLoadedExtension, 'extension=')) {
+                [, $commandLoadedExtension] = explode('=', $commandLoadedExtension);
+                $requiredExtensions[] = $commandLoadedExtension;
+            }
+        }
+    }
+
     $proc = proc_open(
-        [PHP_BINARY, '-r', sprintf('echo (int) extension_loaded(\'%s\');', Swow::class)],
+        [PHP_BINARY, '-r', 'echo serialize(get_loaded_extensions());'],
         [0 => STDIN, 1 => ['pipe', 'w'], 2 => ['redirect', 1]],
         $pipes, null, null
     );
@@ -25,9 +36,58 @@ declare(strict_types=1);
     } while (!feof($pipes[1]));
     $exitCode = proc_get_status($proc)['exitcode'];
     proc_close($proc);
-    $extensionLoaded = $exitCode === 0 && $output === '1';
+    if ($exitCode !== 0) {
+        throw new RuntimeException('Failed to get loaded extensions');
+    }
+    $defaultLoadedExtensions = !empty($output) ? (array) @unserialize($output) : [];
+    $defaultLoadedExtensions = array_map('strtolower', $defaultLoadedExtensions);
+
+    $iniFiles = [];
+    $mainIniFile = php_ini_loaded_file();
+    if ($mainIniFile !== false) {
+        $iniFiles[] = $mainIniFile;
+    }
+    $scannedFiles = php_ini_scanned_files();
+    if ($scannedFiles !== false) {
+        $scannedFiles = explode(',', $scannedFiles);
+        foreach ($scannedFiles as $file) {
+            $file = trim($file);
+            if ($file !== '') {
+                $iniFiles[] = $file;
+            }
+        }
+    }
+    $iniLoadedExtensions = [];
+    foreach ($iniFiles as $iniFile) {
+        $parsedIni = parse_ini_file($iniFile, true);
+        $iniLoadedExtensions = [];
+        array_walk_recursive($parsedIni, static function (mixed $value, mixed $key) use (&$iniLoadedExtensions): void {
+            if ($key === 'extension') {
+                $iniLoadedExtensions[] = basename((string) $value);
+            }
+        });
+    }
+
+    $unloadedExtensions = array_diff(
+        $requiredExtensions,
+        $defaultLoadedExtensions,
+    );
+    $unloadedExtensions = array_diff(
+        $unloadedExtensions,
+        $iniLoadedExtensions,
+    );
+
+    $args = array_slice($argv, 1);
+    $args = array_filter($args, static function ($arg) {
+        return !str_starts_with($arg, '-dextension=') && $arg !== '-d' && !str_starts_with($arg, 'extension=');
+    });
+    $unloadedExtensionsArgs = [];
+    foreach ($unloadedExtensions as $unloadedExtension) {
+        $unloadedExtensionsArgs[] = '-d';
+        $unloadedExtensionsArgs[] = 'extension=' . $unloadedExtension;
+    }
     $proc = proc_open(
-        [PHP_BINARY, ...($extensionLoaded ? [] : ['-d', 'extension=' . strtolower(Swow::class)]), ...$args],
+        [PHP_BINARY, ...$unloadedExtensionsArgs, ...$args],
         [0 => STDIN, 1 => [PHP_OS_FAMILY === 'Windows' ? 'pipe' : 'pty', 'w'], 2 => STDERR],
         $pipes, null, null
     );
