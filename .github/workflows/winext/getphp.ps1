5015c6cd0f39e3c800605e4d0033509b7a9f4051
dixyes <dixyes@gmail.com>
2025-12-31 11:17:31 +0800

    Update ci test PHP versions
    add 8.5, remove 8.0

--- .github/workflows/winext/getphp.ps1
+++ .github/workflows/winext/getphp.ps1
@@ -14,6 +14,7 @@ $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 . "$scriptPath\utils.ps1" -ToolName "getphp" -MaxTry $MaxTry
 
 $guessedVCVers = @{
+    "8.5" = "VS17";
     "8.4" = "VS17";
     "8.3" = "VS16";
     "8.2" = "VS16";
