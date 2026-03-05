16ce7c86f74049ea3617e5bed0b9c6bb04bad990
twosee <twose@qq.com>
2026-03-06 03:53:09 +0800

    Release v1.6.3-alpha.1

--- tools/versions.php
+++ tools/versions.php
@@ -14,10 +14,10 @@ declare(strict_types=1);
 
 return [
     'swow-extension' => [
-        'version' => '1.6.3-dev',
+        'version' => '1.6.3-alpha.1',
     ],
     'swow-library' => [
-        'version' => '1.6.3-dev',
-        'required_extension_version' => '^1.6.3-dev',
+        'version' => '1.6.3-alpha.1',
+        'required_extension_version' => '^1.6.2',
     ],
 ];
